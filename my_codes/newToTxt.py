390696a9766e714415cc9094a860fcc3887e57f1
parkjunho12 <ghdlwnsgh12@naver.com>
2020-01-22 09:37:56 +0900

    뉴스 데이터 파일 업데이트

--- my_codes/newToTxt.py
+++ my_codes/newToTxt.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-file_path = "./news.csv"
+file_path = "../data/news.csv"
 import re
 
 if __name__ == '__main__':
@@ -8,14 +8,22 @@ if __name__ == '__main__':
     datas = pd.read_csv(file_path, engine='python', names=['원본 TITLE', '원본 CONTENT', '요약 TITLE', '요약 CONTENT'])
     origin_content = datas.loc[:, '원본 CONTENT'].values
     sum_content = datas.loc[:, '요약 CONTENT'].values
+    origin_title = datas.loc[:, '원본 TITLE'].values
+    sum_title = datas.loc[:, '요약 TITLE'].values
     index = 0
 
-    f = open("NEWS.txt", 'w',-1 ,"utf-8")
-    for content in origin_content:
+    f = open("../data/NEWS_SUM.txt", 'w',-1 ,"utf-8")
+    print(len(sum_content))
+    for content in sum_content:
         if content == "":
             continue
         index = index + 1
-        print(index)
         print(content)
-        f.write(content+"\n다음 기사에요\n")
+        contents = str(content).split('\n')
+        text = "".join([s for s in str(content).splitlines(True) if s.strip("\r\n")])
+        texts = text.splitlines()
+        for t in texts:
+            if index != len(sum_title):
+                f.write(t)
+        f.write('\n')
     f.close()
